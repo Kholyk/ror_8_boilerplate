c3317f0b2668e82070f673b8739c24cc47c87696
Oleg Kholyk <oleg@kholyk.ru>
2025-02-27 17:26:43 +0300

    FIX: rubocop and slim-lint offences fixes

--- test/minitest_power_assert_test.rb
+++ test/minitest_power_assert_test.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class TestPowerAssert < Minitest::Test
   def test_power_assert_failed
     # assert { '0'.class == '3'.to_i.times.map { |i| i + 1 }.class }
-    refute { '0'.class == '3'.to_i.times.map { |i| i + 1 }.class }
+    assert_not { '0'.instance_of?('3'.to_i.times.map { |i| i + 1 }.class) }
   end
 end
