0c6abf9059298d20b7a0e4e7ba4d2bfca70fd1b0
Oleg Kholyk <oleg@kholyk.ru>
2025-02-27 17:39:54 +0300

    FIX: base tests and Rails/RefuteMethods

--- test/minitest_power_assert_test.rb
+++ test/minitest_power_assert_test.rb
@@ -3,6 +3,8 @@
 class TestPowerAssert < Minitest::Test
   def test_power_assert_failed
     # assert { '0'.class == '3'.to_i.times.map { |i| i + 1 }.class }
-    assert_not { '0'.instance_of?('3'.to_i.times.map { |i| i + 1 }.class) }
+    # rubocop:disable Rails/RefuteMethods
+    refute { '0'.instance_of?('3'.to_i.times.map { |i| i + 1 }.class) }
+    # rubocop:enable Rails/RefuteMethods
   end
 end
