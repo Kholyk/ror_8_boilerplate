d0c9a4f646cc785141bfb883fa4e17a6a15e7291
Oleg Kholyk <oleg@kholyk.ru>
2025-02-27 17:02:46 +0300

    FEAT: Devise Auth gem initial setup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def display_user_email
+    user_signed_in? ? current_user.email : 'Sign in'
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,23 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+window.addEventListener("turbo:load", (event) => {
+  // Sample for using bootstrap Popover on some elements
+  // https://getbootstrap.com/docs/5.3/components/popovers/#enable-popovers
+  const nodesList = document.querySelectorAll('[data-bs-toggle="popover"]');
+  Array.from(nodesList).map(el => new bootstrap.Popover(el, {'trigger': 'focus'}))
+
+  // Offcanvas menu
+  // https://getbootstrap.com/docs/5.3/components/offcanvas/#via-javascript
+  const offCanvasnodelist = document.querySelectorAll('.offcanvas');
+  [...offCanvasnodelist].map(el => new bootstrap.Offcanvas(el));
+  
+  // Dismissable alerts ant notices
+  const dissmissableNodes = document.querySelectorAll('.alert');
+  [...dissmissableNodes].map((dismissable) => {
+    const alert = new bootstrap.Alert(dismissable);
+    // Closes after 2 seconds
+    setTimeout(() => alert.close(), 6000);
+  })
+});
